10ed1cdc2f6f70307e85f63c1589910b985c440b
Noe2781 <108550273+Noe2781@users.noreply.github.com>
2023-03-31 11:24:52 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,5 @@
+ let nombre = prompt ("¿COMO TE LLAMAS?");
+
+ console.log( nombre );
+ document.querySelector("body").innerHTML = " <h1> " + "¿COMO TE LLAMAS?" + "</h1>";
+ document.write("<h2> me llamo "+ nombre +"</h2>");
