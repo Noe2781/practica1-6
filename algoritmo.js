e2faaa17b7f028ee9956f5dd1b1915ab7d65f651
Noe2781 <108550273+Noe2781@users.noreply.github.com>
2023-03-31 12:33:04 -0300

    tercer commit

--- algoritmo.js
+++ algoritmo.js
@@ -1,16 +1,16 @@
- let nombre = prompt ("¿COMO TE LLAMAS?");
+/*  let nombre = prompt ("¿COMO TE LLAMAS?"); */
  let numeroint = prompt ("Ingresa un numero entero");
- let booleano = prompt ("¿Es verdadero o falso?");
+ let booleano = prompt ("Ingresa un dato booleano");
  let numerofloat = prompt ("Ingresa un numero con decimal");
  let letra = prompt ("Ingresa un caracter");
  let palabra = prompt ("Ingresa una palabra");
  
 
- console.log( nombre );
+ /* console.log( nombre );
  document.querySelector("body").innerHTML = " <h1> " + "¿COMO TE LLAMAS?" + "</h1>";
- document.write("<h2> me llamo "+ nombre +"</h2>");
- document.write("<h3> Entero: "+ numeroint + "</h3>");
- document.write("<h4> Booleano " + booleano + "<H4/>");
- document.write("<h5> Decimal "+ numerofloat + "</h5>");
- document.write("<h6> Caracter "+ letra + "</h6>");
- document.write("<h6> Palabra " + palabra + "</h6>" );
+ document.write("<h2> me llamo "+ nombre +"</h2>"); */
+ document.write("<h1> Entero: "+ numeroint + "</h1>");
+ document.write("<h2> Booleano " + booleano + "</h2>");
+ document.write("<h3> Decimal "+ numerofloat + "</h3>");
+ document.write("<h4> Caracter "+ letra + "</h4>");
+ document.write("<h5> Palabra " + palabra + "</h5>" );
