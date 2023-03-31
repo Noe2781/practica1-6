6ce056b7ce39cc95a4562196f49d0fd6a8e81b8f
Noe2781 <108550273+Noe2781@users.noreply.github.com>
2023-03-31 11:55:19 -0300

    segundo commit

--- algoritmo.js
+++ algoritmo.js
@@ -1,5 +1,16 @@
  let nombre = prompt ("¿COMO TE LLAMAS?");
+ let numeroint = prompt ("Ingresa un numero entero");
+ let booleano = prompt ("¿Es verdadero o falso?");
+ let numerofloat = prompt ("Ingresa un numero con decimal");
+ let letra = prompt ("Ingresa un caracter");
+ let palabra = prompt ("Ingresa una palabra");
+ 
 
  console.log( nombre );
  document.querySelector("body").innerHTML = " <h1> " + "¿COMO TE LLAMAS?" + "</h1>";
  document.write("<h2> me llamo "+ nombre +"</h2>");
+ document.write("<h3> Entero: "+ numeroint + "</h3>");
+ document.write("<h4> Booleano " + booleano + "<H4/>");
+ document.write("<h5> Decimal "+ numerofloat + "</h5>");
+ document.write("<h6> Caracter "+ letra + "</h6>");
+ document.write("<h6> Palabra " + palabra + "</h6>" );
